d250d4be18b339298ec0718f5561ce267f54cb40
EmreTy <Emret@t-online.de>
2021-10-08 16:36:47 +0200

    ??

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Password-check")
+
+
+function addPassword(){
+const wert1 = document.querySelector("#passwd1");
+const wert2 = document.querySelector("#passwd2");
+
+if ()
+
+
+
+
+
+
+}
