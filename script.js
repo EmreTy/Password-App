1945d6f498a8a7789f74abf40a7958d6d5951fe9
EmreTy <Emret@t-online.de>
2021-10-09 13:12:47 +0200

    js wert vergleich?

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ console.log("Password-check")
 function addPassword(){
 const wert1 = document.querySelector("#passwd1");
 const wert2 = document.querySelector("#passwd2");
-
-if ()
-
-
-
-
-
+const wert12 = wert1.value;
+const wert21 = wert2.value;
+if (wert12 === wert21){
+switch 
 
 }
