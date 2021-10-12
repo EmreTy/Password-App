a5ca5439e4f74ff4bebdbfd28b9786672ec7c261
EmreTy <Emret@t-online.de>
2021-10-12 08:45:25 +0200

    Password check if equal

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 console.log("Password-check")
 
 
-function addPassword(){
-const wert1 = document.querySelector("#passwd1");
-const wert2 = document.querySelector("#passwd2");
-const wert12 = wert1.value;
-const wert21 = wert2.value;
-if (wert12 === wert21){
-switch (#equal) = ✅
+//Konstante deklarieren für die input-felder
+const password = document.getElementById("#passwd1");
+const passwordConfirm = document.getElementById("#passwd2");
+const passwordText = password.value;
+const passwordTextCon = passwordConfirm.value;
+
+
+//Passwort checken ob gleich(equal)
+function passwordCheck(){
+    const textEqual = document.getElementById("#equal");
+if (passwordText === passwordTextCon) {
+    textEqual.innerText = "✅";
+    }else {
+        textEqual.innerText = "❌";
+    }
 }
+
+passwordConfirm.addEventListener("input", passwordCheck);
