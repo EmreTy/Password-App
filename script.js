2e716244ca7c8eae1b597a36ad0e48d45d66306e
EmreTy <Emret@t-online.de>
2021-10-12 08:49:34 +0200

    Rauten entfernt bei getElementById

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ console.log("Password-check")
 
 
 //Konstante deklarieren für die input-felder
-const password = document.getElementById("#passwd1");
-const passwordConfirm = document.getElementById("#passwd2");
+
+const password = document.getElementById("passwd1");
+const passwordConfirm = document.getElementById("passwd2");
 const passwordText = password.value;
 const passwordTextCon = passwordConfirm.value;
 
 
 //Passwort checken ob gleich(equal)
+
 function passwordCheck(){
-    const textEqual = document.getElementById("#equal");
+    const textEqual = document.getElementById("equal");
 if (passwordText === passwordTextCon) {
     textEqual.innerText = "✅";
     }else {
